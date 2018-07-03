5e745bc99eb903b0f52777ac8d53a17ddd3e781b
Alberto Barradas <abcsds@gmail.com>
2018-07-03 13:41:28 +0200

    Add most relevant sencence for sentence creation

--- Sentence.py
+++ Sentence.py
@@ -2,19 +2,20 @@
 import random
 
 if __name__ == "__main__":
-    filename = 'output/p&p_res.txt'
-    noun = set()
-    verb = set()
+    filename = 'output/lk_emo.txt'
+    noun = []
+    verb = []
     whole_words_set = {word.rstrip() for word in open(filename)}
 
     for word in whole_words_set:
         if word.endswith('ing'):
-            verb.add(word)
+            verb.append(word)
         else:
-            noun.add(word)
+            noun.append(word)
 
     noun_list = list(noun)
     verb_list = list(verb)
+    print(noun_list[0], verb[0], noun_list[1])
     for i in range(10):
         sentence = "%s %s %s" % (random.choice(noun_list),
                                  random.choice(verb_list),
